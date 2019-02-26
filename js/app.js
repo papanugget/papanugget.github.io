6d6ed6c45c7935c0ffa1b9278f7133207f94cc08
papanugget <evilgenius04@gmail.com>
2019-02-26 10:52:50 -0500

    accepted incoming js changes from remote master

--- js/app.js
+++ js/app.js
@@ -69,7 +69,6 @@
 
 // ES6 class refactor
 class TypeWriter {
-<<<<<<< HEAD
     constructor(txtElement, words, wait = 3000) {
         this.txtElement = txtElement;
         this.words = words;
@@ -102,40 +101,6 @@ class TypeWriter {
         }
         setTimeout(() => this.type(), typeSpeed);
     }
-=======
-  constructor(txtElement, words, wait = 3000) {
-      this.txtElement = txtElement;
-      this.words = words;
-      this.txt = '';
-      this.wordIndex = 0;
-      this.wait = parseInt(wait, 10);
-      this.type();
-      this.isDeleting = false;
-  }
-  type() {
-      const current = this.wordIndex % this.words.length;
-      const fullText = this.words[current];
-      if(this.isDeleting) {
-          this.txt = fullText.substring(0, this.txt.length - 1);
-      } else {
-          this.txt = fullText.substring(0, this.txt.length + 1);
-      }
-      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-      let typeSpeed = 300;
-      if(this.isDeleting) {
-          typeSpeed /= 2;
-      }
-      if(!this.isDeleting && this.txt === fullText ) {
-          typeSpeed = this.wait;
-          this.isDeleting = true;
-      } else if(this.isDeleting && this.txt === '') {
-          this.isDeleting = false;
-          this.wordIndex++;
-          typeSpeed = 500;
-      }
-      setTimeout(() => this.type(), typeSpeed);
-  }
->>>>>>> f11f4f5526276e869678946bf8e6df676b5f7904
 }
 
 // init on DOM load
@@ -143,17 +108,9 @@ document.addEventListener('DOMContentLoaded', init);
 
 // init app
 function init() {
-<<<<<<< HEAD
     const txtElement = document.querySelector('.txt-type');
     const words = JSON.parse(txtElement.getAttribute('data-words'));
     const wait = txtElement.getAttribute('data-wait');
     // init typewriter
     new TypeWriter(txtElement, words, wait);
-=======
-  const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
-  // init typewriter
-  new TypeWriter(txtElement, words, wait);
->>>>>>> f11f4f5526276e869678946bf8e6df676b5f7904
 }
